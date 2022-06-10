a2f1f83a721b792a6c245dbdca562a6b856bcc62
Liam Imray <liamimray@Liams-MacBook-Pro.local>
2022-06-10 14:56:15 +0100

    Removed unused folder

--- components/EligibilityForm.js
+++ components/EligibilityForm.js
@@ -23,36 +23,36 @@ const EligibilityForm = ({ show }) => {
       <form>
         <div className={styles.efc__input__cb}>
           <label htmlFor='checkbox1'>Are you 18+ ?</label>
-          <input type='checkbox' id='checkbox1'name='age' placeholder='Placeholder text here' onChange={(e)=>{
+          <input type='checkbox' id='checkbox1' name='age' placeholder='Placeholder text here' onChange={(e) => {
             setEligibility(prevState => {
               return {
                 ...prevState,
                 [e.target.name]: e.target.checked
               }
             })
-          }} required/>
+          }} required />
         </div>
         <div className={styles.efc__input__cb} >
           <label htmlFor='checkbox2'>Are you currently unemployed?</label>
-          <input type='checkbox' name='unemployed' id='checkbox2' placeholder='Placeholder text here' onChange={(e)=>{
+          <input type='checkbox' name='unemployed' id='checkbox2' placeholder='Placeholder text here' onChange={(e) => {
             setEligibility(prevState => {
               return {
                 ...prevState,
                 [e.target.name]: e.target.checked
               }
             })
-          }} required/>
+          }} required />
         </div>
         <div className={styles.efc__input__cb}>
           <label htmlFor='checkbox3'>Are you in receipt of Universal Credit?</label>
-          <input type='checkbox' name='uc' id='checkbox3' placeholder='Placeholder text here' onChange={(e)=>{
+          <input type='checkbox' name='uc' id='checkbox3' placeholder='Placeholder text here' onChange={(e) => {
             setEligibility(prevState => {
               return {
                 ...prevState,
                 [e.target.name]: e.target.checked
               }
             })
-          }} required/>
+          }} required />
         </div>
         <div className={styles.efcFormBtn}>
           <button className={eligibility.age && eligibility.unemployed && eligibility.uc ? styles.eligible : styles.disabled}>{eligibility.age && eligibility.unemployed && eligibility.uc ? 'Find a Job' : 'Sorry you are not eligible'}</button>
